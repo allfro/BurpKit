b97e0a5c77af8a98c828281acb9fcc4fa66e4d1b
allfro <ndouba@redcanari.com>
2015-06-23 16:29:27 -0400

    Created a loose TrustManager to support untrusted certificates without prompting the user.

--- src/main/java/com/redcanari/net/security/TrustManager.java
+++ src/main/java/com/redcanari/net/security/TrustManager.java
@@ -1,17 +1,33 @@
+/*
+ * BurpKit - WebKit-based penetration testing plugin for BurpSuite
+ * Copyright (C) 2015  Red Canari, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.redcanari.net.security;
 
 import javax.net.ssl.X509TrustManager;
 import java.security.cert.X509Certificate;
 
 /**
- * DO NOT USE IN PRODUCTION!!!!
- *
- * This class will simply trust everything that comes along.
- *
- * @author frank
- *
+ * @author  Nadeem Douba
+ * @version 1.0
+ * @since   01-01-2014
  */
 public class TrustManager implements X509TrustManager {
+
     public X509Certificate[] getAcceptedIssuers() {
         return new X509Certificate[0];
     }
