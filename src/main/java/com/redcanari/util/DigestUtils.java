5b7988e3cfb22cc6f9e053786c16b4d8e34383b0
allfro <ndouba@redcanari.com>
2015-06-23 16:33:10 -0400

    Added helper classes for various things.

--- src/main/java/com/redcanari/util/DigestUtils.java
+++ src/main/java/com/redcanari/util/DigestUtils.java
@@ -1,3 +1,21 @@
+/*
+ * BurpKit - WebKit-based penetration testing plugin for BurpSuite
+ * Copyright (C) 2015  Red Canari, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.redcanari.util;
 
 import java.math.BigInteger;
@@ -6,10 +24,18 @@ import java.security.NoSuchAlgorithmException;
 
 
 /**
- * Created by ndouba on 2014-06-02.
+ * @author  Nadeem Douba
+ * @version 1.0
+ * @since   2014-06-02.
  */
 public class DigestUtils {
 
+    /**
+     * Returns the md5 message digest of byte array.
+     *
+     * @param content   the byte array that requires digesting
+     * @return  the md5 digest of the byte array as a {@code java.lang.String}
+     */
     public static String toDigest(byte[] content) {
         String md5String = null;
         try {
--- src/main/java/com/redcanari/util/HttpUtils.java
+++ src/main/java/com/redcanari/util/HttpUtils.java
@@ -1,22 +1,43 @@
+/*
+ * BurpKit - WebKit-based penetration testing plugin for BurpSuite
+ * Copyright (C) 2015  Red Canari, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.redcanari.util;
 
 import java.io.UnsupportedEncodingException;
-import java.math.BigInteger;
 import java.net.URL;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
 
 /**
- * Created by ndouba on 2014-06-02.
+ * @author  Nadeem Douba
+ * @version 1.0
+ * @since   2014-06-02
  */
 public class HttpUtils {
 
-
+    /**
+     * Returns a string containing a properly formed URL with port and scheme information.
+     * @param url the {@link java.net.URL} that needs normalization.
+     * @return a {@link java.lang.String} that contains a well-formed URL.
+     */
     public static String normalizeUrl(URL url) {
         int port = url.getPort();
         String portString = "";
@@ -53,6 +74,12 @@ public class HttpUtils {
         return protocol + "://" + url.getHost().toLowerCase() + portString + file;
     }
 
+    /**
+     * Returns true if the URL string begins with the standard HTTP protocol schemes.
+     *
+     * @param url a string containing a URL string.
+     * @return a boolean indicating whether the string starts with "http://" or "https://"
+     */
     public static boolean isHttpURL(String url) {
         return url != null && (url.startsWith("http://") || url.startsWith("https://"));
     }
