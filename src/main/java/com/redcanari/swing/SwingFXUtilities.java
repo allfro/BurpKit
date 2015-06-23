06de46978001b9f381f76a4257415160aceb4e20
allfro <ndouba@redcanari.com>
2015-06-23 16:29:57 -0400

    A simple helper class that supports Swing-FX messaging.

--- src/main/java/com/redcanari/swing/SwingFXUtilities.java
+++ src/main/java/com/redcanari/swing/SwingFXUtilities.java
@@ -1,3 +1,21 @@
+/*
+ * BurpKit - WebKit-based penetration testing plugin for BurpSuite
+ * Copyright (C) 2015  Red Canari, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.redcanari.swing;
 
 import javafx.application.Platform;
@@ -12,7 +30,7 @@ import java.util.concurrent.FutureTask;
 /**
  * Created by ndouba on 14-12-09.
  */
-public class SwingUtilitiesX {
+public class SwingFXUtilities {
 
     public static <T> FutureTask<T> invokeLater(Callable<T> callable, JSObject callback) {
         FutureTask<T> task = new FutureTask<>(callable);
