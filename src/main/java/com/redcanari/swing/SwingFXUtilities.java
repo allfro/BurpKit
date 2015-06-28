c6eca6cda57fe8c251502361fd249332c2723ad3
allfro <ndouba@redcanari.com>
2015-06-28 14:56:31 -0400

    Fixed a couple bugs

--- src/main/java/com/redcanari/swing/SwingFXUtilities.java
+++ src/main/java/com/redcanari/swing/SwingFXUtilities.java
@@ -22,62 +22,34 @@ import javafx.application.Platform;
 import netscape.javascript.JSObject;
 
 import javax.swing.*;
+import java.awt.*;
 import java.lang.reflect.InvocationTargetException;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.FutureTask;
+import java.util.*;
+import java.util.concurrent.*;
 
 /**
  * Created by ndouba on 14-12-09.
  */
 public class SwingFXUtilities {
 
-    public static <T> FutureTask<T> invokeLater(Callable<T> callable, JSObject callback) {
-        FutureTask<T> task = new FutureTask<>(callable);
-
+    public static <T> void invokeLater(Callable<T> callable, JSObject callback) {
         SwingUtilities.invokeLater(() -> {
             try {
                 final T result = callable.call();
                 if (callback != null) {
-                    Platform.runLater(() -> {
-                        callback.setMember("this", callback);
-                        callback.call("this", result);
-                        callback.removeMember("this");
-                    });
+                    Platform.runLater(() -> callback.call("call", null, result));
                 }
             } catch (Exception e) {
                 e.printStackTrace();
             }
-
         });
-
-        return task;
     }
 
-    public static <T> FutureTask<T> invokeLater(Callable<T> callable) {
+    public static <T> T invokeAndWait(Callable<T> callable) throws TimeoutException, ExecutionException, InterruptedException {
+        //blocks until future returns
         FutureTask<T> task = new FutureTask<>(callable);
-
         SwingUtilities.invokeLater(task);
-
-        return task;
-    }
-
-    public static <T> T invokeAndWait(Callable<T> callable) throws InterruptedException,
-            InvocationTargetException {
-        try {
-            //blocks until future returns
-            return invokeLater(callable).get();
-        } catch (ExecutionException e) {
-            Throwable t = e.getCause();
-
-            if (t instanceof RuntimeException) {
-                throw (RuntimeException) t;
-            } else if (t instanceof InvocationTargetException) {
-                throw (InvocationTargetException) t;
-            } else {
-                throw new InvocationTargetException(t);
-            }
-        }
+        return task.get();
     }
 
 
